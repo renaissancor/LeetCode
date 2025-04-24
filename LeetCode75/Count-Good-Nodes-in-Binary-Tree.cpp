b3501fc643159a49e443f54518bc967e32f11562
Jae Heung Park <56800332+renaissancor@users.noreply.github.com>
2025-04-24 19:46:02 +0900

    [1448] [Time Beats: 34.50%] [Memory Beats: 27.35%] - LeetPush

---
+++ LeetCode75/Count-Good-Nodes-in-Binary-Tree.cpp
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public: 
+    int cnt; 
+    void isGood(TreeNode* node, int big){
+
+        if(node->val >= big){
+            big = node->val; 
+            ++cnt;             
+        }
+
+        if(node->left){
+            isGood(node->left, big);
+        }
+
+        if(node->right){
+            isGood(node->right, big); 
+        }
+    }
+
+public:
+    int goodNodes(TreeNode* root) {
+        cnt = 0; 
+
+        if(root){
+            isGood(root, root->val); 
+        }        
+
+        return cnt;                
+    }
+};
