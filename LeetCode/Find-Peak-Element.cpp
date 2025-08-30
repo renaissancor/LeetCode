d0d25e33ae0bfc66bed6e50ad1d22bd96d8ccdf4
Jae Heung Park <56800332+renaissancor@users.noreply.github.com>
2025-08-30 13:28:51 +0900

    [162] [Time Beats: 100.00%] [Memory Beats: 32.67%] - LeetPush

--- LeetCode/Find-Peak-Element.cpp
+++ LeetCode/Find-Peak-Element.cpp
@@ -6,28 +6,14 @@ public:
 
         int l = 0; 
         int r = n - 1; 
-        int i = n / 2; 
 
-        while (l < i && i < r) {
-            i = l + (r - l) / 2; 
-            if (i == 0 || nums[i] > nums[i - 1]) {
-                if (i == n - 1 || nums[i] > nums[i + 1])
-                    return i; 
-                l = i + 1; 
-            } else {
-                r = i - 1; 
-            }
-        }
-
-        if (i == 0 || nums[i] > nums[i - 1]) {
-            while (i != n - 1 && nums[i] < nums[i + 1]) ++i; 
-            return i; 
-        } else {
-            while (i != 0 && nums[i] < nums[i - 1]) --i; 
-            return i; 
+        while (l < r) {
+            int i = l + (r - l) / 2; 
+            if(nums[i] > nums[i + 1]) r = i; 
+            else l = i + 1;
         }
             
 
-        return i; 
+        return l; 
    }
 };
