4604c6e83dfed2e63ec8450d7aa8da0865671c58
Jae Heung Park <56800332+renaissancor@users.noreply.github.com>
2025-08-31 16:11:25 +0900

    [13] [Time Beats: 75.50%] [Memory Beats: 19.17%] - LeetPush

--- LeetCode75/Roman-to-Integer.cpp
+++ LeetCode75/Roman-to-Integer.cpp
@@ -3,55 +3,15 @@ public:
     int romanToInt(string s) {
         
         int ans = 0; 
-        int n = s.size();
-        for(int i = n - 1; i >= 0; --i) {
-            switch(s[i]) {
-                case 'I': 
-                    ans += 1;                
-                    break; 
-                case 'V': 
-                    ans += 5; 
-                    if (i > 0 && s[i-1] == 'I') {
-                        ans -= 1; 
-                        --i; 
-                    }
-                    break; 
-                case 'X': 
-                    ans += 10; 
-                    if (i > 0 && s[i-1] == 'I') {
-                        ans -= 1; 
-                        --i; 
-                    }                    
-                    break; 
-                case 'L': 
-                    ans += 50; 
-                    if (i > 0 && s[i-1] == 'X') {
-                        ans -= 10; 
-                        --i; 
-                    }
-                    break; 
-                case 'C': 
-                    ans += 100; 
-                    if (i > 0 && s[i-1] == 'X') {
-                        ans -= 10; 
-                        --i; 
-                    }
-                    break; 
-                case 'D': 
-                    ans += 500; 
-                    if (i > 0 && s[i-1] == 'C') {
-                        ans -= 100; 
-                        --i; 
-                    }
-                    break; 
-                case 'M': 
-                    ans += 1000; 
-                    if (i > 0 && s[i-1] == 'C') {
-                        ans -= 100; 
-                        --i; 
-                    }
-                    break; 
-            }
+        unordered_map<char, int> ump = {
+            {'I', 1}, {'V', 5}, {'X', 10}, {'L', 50},
+            {'C', 100}, {'D', 500}, {'M', 1000}
+        };
+
+        for (int i = 0; i < s.size(); ++i) {
+            if (i + 1 < s.size() && ump[s[i]] < ump[s[i+1]]) 
+                ans -= ump[s[i]]; 
+            else ans += ump[s[i]]; 
         }
 
         return ans; 
