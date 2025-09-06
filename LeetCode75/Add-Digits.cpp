226c0b85b638311695479c15d0015eae6fd33ec2
Jae Heung Park <56800332+renaissancor@users.noreply.github.com>
2025-09-06 14:54:52 +0900

    [258] [Time Beats: 100.00%] [Memory Beats: 9.81%] - LeetPush

---
+++ LeetCode75/Add-Digits.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int addDigits(int num) {
+
+        while (num > 9) {
+            int tmp = 0; 
+            while (num) {
+                tmp += (num % 10); 
+                num /= 10; 
+            }
+            num = tmp; 
+        }
+        
+        return num; 
+    }
+};
